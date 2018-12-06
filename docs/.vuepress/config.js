b1b94b484e5966290d42bd22c32c5d8558b14886
Ali Bashi <alisalad123@gmail.com>
2018-12-06 13:10:54 +1100

    languaes added

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,6 +2,12 @@ module.exports = {
   title: 'Dastuurka',
   description: 'Jamhuuriyadda Federaalka ee Soomaaliya',
 
+  //Displays options for langugaes
+  locales: {
+    "/": { lang: "Soomaali" },
+    "/lang/": { lang: "English" }
+  },
+
   head: [
     ['link', { rel: 'icon', href: '/somalia.png' }]
   ]
