19741903767d3ffa07fc887889492d2ce1b986e8
Ali Bashi <alisalad123@gmail.com>
2018-12-06 12:55:44 +1100

    header

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,24 @@
 module.exports = {
   title: 'Dastuurka',
-  description: 'Jamhuuriyadda Federaalka ee Soomaaliya'
+  description: 'Jamhuuriyadda Federaalka ee Soomaaliya',
+  //Displays options for langugaes
+  locales: {
+    "/": { lang: "Soomaali" },
+    "/lang/": { lang: "English" }
+  },
+  //Displays favicon
+  head: [["link", { rel: "icon", href: "/somalia.png" }]],
+
+  themeConfig: {
+
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Dastuurka', link: '/dastuurka/' },
+      { text: 'Guide', link: '/guide/' },
+      {}
+    ],
+
+    sidebar: 'auto'
+
+  }
 }
