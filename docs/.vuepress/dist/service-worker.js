ad5c96d635616d7281a7d0979798899477b03ada
Ali Bashi <alisalad123@gmail.com>
2018-12-30 03:09:35 +1100

    PWA

--- docs/.vuepress/dist/service-worker.js
+++ docs/.vuepress/dist/service-worker.js
@@ -21,51 +21,51 @@ importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox
 self.__precacheManifest = [
   {
     "url": "404.html",
-    "revision": "a2bda23f4f5254ef56c47b3f42ee4d8b"
+    "revision": "bebd510724d5193fc7ecdbb308bc221a"
   },
   {
-    "url": "assets/css/0.styles.a5e41e54.css",
-    "revision": "ebd013e8259596d82a47ec5161061232"
+    "url": "assets/css/0.styles.7e0e4fb9.css",
+    "revision": "2d712e20cd10a9d6807bb334a0d79fb4"
   },
   {
     "url": "assets/img/search.83621669.svg",
     "revision": "83621669651b9a3d4bf64d1a670ad856"
   },
   {
-    "url": "assets/js/2.a4cf675e.js",
-    "revision": "1c6b048b81dfea0ce9854bdbb68e1bc2"
+    "url": "assets/js/2.25c59790.js",
+    "revision": "7a8b97b8e63294f87068e6f312a48f48"
   },
   {
-    "url": "assets/js/3.b5a7fee5.js",
-    "revision": "9e99d03f83b1fc1c100587696387c8c5"
+    "url": "assets/js/3.d1bff602.js",
+    "revision": "356e437cf051586a5966a45d7d699b68"
   },
   {
-    "url": "assets/js/4.7508c596.js",
-    "revision": "f97ad097a881db3b1b99e33342fe0b90"
+    "url": "assets/js/4.399150da.js",
+    "revision": "e38648487829d900466351a7effceb64"
   },
   {
-    "url": "assets/js/5.b6f5b1fb.js",
-    "revision": "aa8eeae9218b74c65623fb4e40a698d6"
+    "url": "assets/js/5.7c522f4a.js",
+    "revision": "04cb9c06d52eb357539eab36bd36493b"
   },
   {
-    "url": "assets/js/6.6d380a7b.js",
-    "revision": "28577a95a14251ad7256258654b87657"
+    "url": "assets/js/6.e3081026.js",
+    "revision": "bb8f34f697fe269d69cfd39503de7754"
   },
   {
-    "url": "assets/js/7.7ade1ace.js",
-    "revision": "a9a55381e6898ef0becfc7d1dfbe9ab2"
+    "url": "assets/js/7.92243b67.js",
+    "revision": "4ec5195d942f0779a4588a9d28c53ce7"
   },
   {
-    "url": "assets/js/8.db55b669.js",
-    "revision": "4a0b57e110b2e4d1f8fc71d5522dbc95"
+    "url": "assets/js/8.9b25be2b.js",
+    "revision": "65386ef73f2fac4e8b197997f2ed6463"
   },
   {
-    "url": "assets/js/9.fa5db5a6.js",
-    "revision": "97dd40a3159ffdfa68b0bb3d51b64fb6"
+    "url": "assets/js/9.f4d56fc1.js",
+    "revision": "d5a44794f076ff5417c90ddbf68094b4"
   },
   {
-    "url": "assets/js/app.a148651b.js",
-    "revision": "4dbbebb5e6f8469fb7c571c511eef9f8"
+    "url": "assets/js/app.6d000fe7.js",
+    "revision": "a8fb53e609440a8c1873d1766424ec0c"
   },
   {
     "url": "calanka.png",
@@ -77,27 +77,59 @@ self.__precacheManifest = [
   },
   {
     "url": "dastuurka/index.html",
-    "revision": "ddb512bf7bcd074774b79c0bebdbac33"
+    "revision": "73cfaf82f912695fab82d1f86c8aa8c6"
   },
   {
     "url": "english/index.html",
-    "revision": "e4f96ed63b0253f8ceeab0396efc6ccb"
+    "revision": "af121feeff23b0837d9aea970446f2d6"
   },
   {
     "url": "guide/index.html",
-    "revision": "3726fcef0d83a15f6858fe7e7d46c6fc"
+    "revision": "48ed7342b22623a7d0009413812872c7"
   },
   {
     "url": "guidebook.jpg",
     "revision": "544d37beeefe0244c6d8a05b8849b3db"
   },
   {
+    "url": "icon-128x128.png",
+    "revision": "b020ae26f2a84bdd4f70ade774dfe7bd"
+  },
+  {
+    "url": "icon-144x144.png",
+    "revision": "3d08a812cda5cd90dfb32bd2411d43ac"
+  },
+  {
+    "url": "icon-152x152.png",
+    "revision": "258d30456c8754bacfe1b818501abce1"
+  },
+  {
+    "url": "icon-192x192.png",
+    "revision": "75c423318de04f38513bd91d95e7d157"
+  },
+  {
+    "url": "icon-384x384.png",
+    "revision": "cb82ab198e2b628bec5b56786147804b"
+  },
+  {
+    "url": "icon-512x512.png",
+    "revision": "9e199d7037c4ad81d161442258976472"
+  },
+  {
+    "url": "icon-72x72.png",
+    "revision": "5d0bf2830706937b5e694391885ef450"
+  },
+  {
+    "url": "icon-96x96.png",
+    "revision": "22b17266f3b2a7ecfb07578b7022d09d"
+  },
+  {
     "url": "index.html",
-    "revision": "15bf82658cd42a4c702c5ac19b3261a4"
+    "revision": "af9ca5dec9cc99f0815227ec40b5e58b"
   },
   {
     "url": "lang/index.html",
-    "revision": "123c31475509f2c26a5f02fa87d62f0b"
+    "revision": "92e98ab82bd6cc6f6703a1887c782bf2"
   },
   {
     "url": "provisional_constitution.png",
@@ -109,7 +141,7 @@ self.__precacheManifest = [
   },
   {
     "url": "xeerhoosaadka/index.html",
-    "revision": "ff80c0ba1f9f7d691847bfa1eb2b9a05"
+    "revision": "2c8ad3f4a6c9ec119a1f5a5eba790e65"
   }
 ].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
