f86af50b7515b2244463e57e8c6759da3efbecec
Ali Bashi <alisalad123@gmail.com>
2019-10-16 16:44:58 +1100

    algolia

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -6,9 +6,7 @@ module.exports = {
   locales: {
     "/": { lang: "Soomaali" },
     "/lang/": { lang: "English" },
-    "/arabic/": { lang: "Arabic" },
-
-
+    "/arabic/": { lang: "Arabic" }
   },
 
   plugins: ['@vuepress/pwa'],
@@ -25,24 +23,13 @@ module.exports = {
     ]
   ],
   ga: 'UA-130701702-1',
-  /*description: 'Dastuurka Soomaaliya.',*/
 
   themeConfig: {
 
-
-    /*algolia: {
+    algolia: {
       apiKey: '439dcbf329efb458689c0c99ec033fcd',
       appId: '2LBWW686RF',
       indexName: 'dev_DASTUURKA',
-      }
-    },*/
-
-    lastUpdated: 'Last Updated',
-
-    serviceWorker: {
-      updatePopup: true,
-      message: "New content is available.",
-      buttonText: "Refresh"
     },
 
 
